3d4dea9d3454c395df133db3e8156f5f854c744a
loctodale <thang336655@gmail.com>
2025-06-25 17:46:20 +0700

    update

--- components/about/development-history/index.tsx
+++ components/about/development-history/index.tsx
@@ -5,10 +5,10 @@ import Image from "next/image";
 const DevelopmentHistory = () => {
     return (
         <section className="w-full h-full mt-32 md:mt-12">
-            <div className="w-full max-w-screen-xl mx-auto pl-6 sm:px-6 lg:px-20 h-full">
-                <div className="flex flex-col-reverse lg:flex-row items-center justify-between w-full h-full gap-8">
+            <div className="w-full max-w-screen-xl mx-auto sm:px-6 lg:px-20 h-full">
+                <div className="flex flex-col-reverse lg:flex-row items-center justify-between w-full h-full gap-4">
                     {/* Timeline & title section */}
-                    <div className="w-full flex flex-col items-center lg:items-start">
+                    <div className="w-full flex flex-col items-center lg:items-start px-4">
                         <h2 className="uppercase font-semibold text-[1.5rem] sm:text-[2rem] md:text-[2.5rem] lg:text-[2.75rem] text-[#172345] mb-4 text-center lg:text-left whitespace-nowrap">
                             Lịch sử phát triển
                         </h2>
@@ -16,13 +16,13 @@ const DevelopmentHistory = () => {
                     </div>
 
                     {/* Image section */}
-                    <div className="w-full lg:w-1/2 flex justify-center">
+                    <div className="w-full h-1/3 flex justify-center my-8 ">
                         <Image
-                            src="/assets/development-history.png"
+                            src="/assets/development.jpg"
                             alt="bg-dev-history"
-                            width={800}
-                            height={500}
-                            className="max-w-full h-auto"
+                            width={400}
+                            height={300}
+                            className="max-w-full"
                         />
                     </div>
                 </div>
